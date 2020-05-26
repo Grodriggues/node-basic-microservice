f780a4dc52184e05d4eb9d664b30e0e57a31be8f
Gabriel Rodrigues <gabrieloliveira12488@gmail.com>
2020-05-26 16:05:19 -0300

    adding infra and kube

--- moderation/index.js
+++ moderation/index.js
@@ -4,12 +4,14 @@ const axios = require("axios");
 
 const app = express();
 
+app.use(bodyParser.json())
+
 app.post("/events", async(req, res) => {
   const { type, data } = req.body;
 
   if (type === "CommentCreated") {
     const status = data.content.includes("orange") ? "rejected" : "approved";
-    await axios.post("http://localhost:4005/events",{
+    await axios.post("http://event-bus-srv:4005/events",{
       type:"CommentModerated",
       data:{
         id:data.id,
